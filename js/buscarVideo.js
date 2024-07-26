bafdf837cf4b153a135e7314485a2526b61c14e3
Rosangela <96451367+Rommendes@users.noreply.github.com>
2024-07-26 19:27:34 -0300

    up

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,24 +1,16 @@
 import { conectaApi } from "./conectaApi.js";
 import  { constroiCard } from "./mostrarVideos.js";
-async function buscaVideo(evento){
+
+ async function buscaVideo(evento){
     evento.preventDefault();
     
+    const busca = await conectaApi.buscarVideo();
+
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
     
     const lista = document.querySelector("[data-lista] ");
     busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemnto.imagem)));
-
-
-    // try{
-    //     const busca = await conectaApi.buscaVideo(dadosDePesquisa)
-    //     console.log("Busca Vídeo", busca)
-    // }catch (error){
-    //     console.error(error.message);
-    //     alert(error.message);
-    // }
-    
-
-    
+     
 }
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]")
 botaoDePesquisa.addEventListener("click", evento => buscaVideo(evento))
